86d10dc7846ec00deaa64aef6c6962ad281c2314
Costas Kiteou <ckiteou22@gmail.com>
2017-06-19 15:51:45 +0100

    Add hash within an array to include cohort, allowed print(students)method to communcate its intent

--- directory.rb
+++ directory.rb
@@ -1,23 +1,25 @@
 students = [
-  "Dr. Hannibal Lecter",
-  "Darth Vader",
-  "Nurse Ratched",
-  "Michael Corleone",
-  "Alex DeLarge",
-  "The Wicked Witch of the West",
-  "Terminator",
-  "Freddy Krueger",
-  "The Joker",
-  "Joffrey Baraheon",
-  "Norman Bates"
+  {name: "Dr. Hannibal Lecter", cohort: :november},
+  {name: "Darth Vader", cohort: :november},
+  {name: "Nurse Ratched", cohort: :november},
+  {name: "Michael Corleone", chort: :november},
+  {name: "Alex DeLarge", cohort: :november},
+  {name: "The Wicked Witch of the West", cohort: :november},
+  {name: "Terminator", cohort: :november},
+  {name: "Freddy Krueger", cohort: :november},
+  {name: "The Joker", cohort: :november},
+  {name: "Joffrey Baraheon", cohort: :november},
+  {name: "Norman Bates", cohort: :november}
 ]
 def print_header
-  puts  "The Students of Villans Academy"
+  puts  "The Students of Villains Academy"
   puts  "-------------"
 end
 
-def print(names)
-  puts names
+def print(student)
+  student.each do |student|
+    puts "#{student[:name]} (#{student[:cohort]} cohort)"
+  end
 end
 
 def print_footer(names)
