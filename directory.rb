2c9a26b41e4585f657c1a90a224284851e437c2c
Costas Kiteou <ckiteou22@gmail.com>
2017-06-25 13:25:55 +0100

    For menu items 3 'Save the list' and 4 'Load the list') changed the script to allow more flexibility by asking for the filename if the user chooses these menu items

--- directory.rb
+++ directory.rb
@@ -11,8 +11,8 @@ def print_menu
   puts "Please select a number from the following options:"
   puts "1. Input the students"
   puts "2. Show the students"
-  puts "3. Save the list to students.csv"
-  puts "4. Load the list from the students.csv"
+  puts "3. Save the list"
+  puts "4. Load the list"
   puts "9. Exit"
 end
 
@@ -23,10 +23,9 @@ def process(selection)
   when "2"
     show_students
   when "3"
-    save_students
+    save_file
   when "4"
-    load_students
-    load_student_count_message
+    load_file
   when "9"
     exit # this will cause the program to terminate
   else
@@ -39,7 +38,6 @@ def try_load_students
   filename = ARGV.first # first argument from the command line
    if filename.nil?
      load_students
-     puts "Loaded #{@students.count} from 'students.csv'"
    else
      if_load_file_exists(filename)
   end
@@ -48,10 +46,23 @@ end
 def if_load_file_exists(filename)
   if File.exists?(filename) # if it exists
     load_students(filename)
-    puts "Loaded #{@students.count} from #{filename}"
   else #if it doesn't exists
     puts "Sorry, #{filename} doesn't exist."
     exit # quit the program
+  end
+end
+
+def load_file
+  puts "Which file would you like to load the data from?"
+  filename = gets.chomp
+  while !filename.empty?
+    if File.exists?(filename) # if it exists
+      load_students(filename)
+      break
+    else
+      puts "Sorry, #{filename} doesn't exist. Please try again or press 'enter' to exit"
+      filename = gets.chomp
+    end
   end
 end
 
@@ -63,16 +74,32 @@ def load_students(filename = "students.csv")
     add_students_to_var(name, age, hobby, postcode, c_o_b, cohort)
     @load_student_count += 1
   end
+  load_student_count_message(filename)
   file.close
 end
 
-def load_student_count_message
-  puts "#{@load_student_count} students loaded"
-  puts
+def load_student_count_message(filename)
+  puts "--------------------------------------------------"
+  puts "#{@load_student_count} students loaded from '#{filename}'"
+  puts "--------------------------------------------------"
 end
 
-def save_students
-  file = File.open("students.csv", "w")
+def save_file
+  puts "Which file would you like to save the data to?"
+  filename = gets.chomp
+  while !filename.empty?
+    if File.exists?(filename) # if it exists
+      save_students(filename)
+      break
+    else
+      puts "Sorry, '#{filename}'' doesn't exist. Please try again or press 'enter' to exit"
+      filename = gets.chomp
+    end
+  end
+end
+
+def save_students(filename)
+  file = File.open(filename, "w")
   count = 0
   @students.each do |student|
     student_data = [student[:name], student[:age], student[:hobby], student[:postcode], student[:c_o_b], student[:cohort]]
@@ -80,8 +107,9 @@ def save_students
     file.puts csv_line
     count += 1
   end
-  puts "#{count} students saved"
-  puts
+  puts "--------------------------------------------------"
+  puts "#{count} students saved to '#{filename}'"
+  puts "--------------------------------------------------"
   file.close
 end
 
@@ -130,14 +158,6 @@ def show_students
   print_footer
 end
 
-def show_students
-  print_header
-  #shorter_than_12 = select_name_shorter_than_12(@students)
-  #shorter_than_12_and_only_char = select_name_starting_with_char(shorter_than_12)
-  print_students_list
-  print_footer
-end
-
 def print_header
   puts  "The Students of Villains Academy".center(140)
   puts  "--------------------------------".center(140)
@@ -157,9 +177,9 @@ def print_students_list
 end
 
 def print_footer
-  puts
+  puts "--------------------------------------------------"
   puts "Overall, we have #{@students.count} great students"
-  puts
+  puts "--------------------------------------------------"
 end
 
 try_load_students
